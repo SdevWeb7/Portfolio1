8281059a9a4a4f972bf45ce4870d37f99620fe52
SdevWeb7 <sd.developper@gmail.com>
2024-09-10 13:59:44 +0200

    fix bug

--- app/profil/page.js
+++ app/profil/page.js
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "/lib/auth";
 import { redirect } from "next/navigation";
 import { EditVideo } from "./EditVideo";
+import prisma from "/lib/prisma";
 
 export default async function Profil () {
    const session = await getServerSession(authOptions)
