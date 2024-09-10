c2e7c35755eaf3c5156bf1dbe59ac2c84a04b129
SdevWeb7 <sd.developper@gmail.com>
2024-09-10 13:10:00 +0200

    fix bug prod

--- actions/searchAction.js
+++ actions/searchAction.js
@@ -1,12 +1,9 @@
 "use server"
 
-import { serverAction } from "./safeActions";
-import { searchSchemas } from "/lib/yupSchemas";
+
 import prisma from "/lib/prisma";
 
-export const searchVideo = serverAction(
-   searchSchemas,
-   async function ({searchKey}) {
+export const searchVideo = async function ({searchKey}) {
       const result = await prisma.video.findMany({
          where: {
             name: {
@@ -22,4 +19,3 @@ export const searchVideo = serverAction(
       })
       return result
    }
-)
